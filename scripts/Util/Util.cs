1374bd8dd5bbba5927b024e4d13cd8d7ddb9a38e
JaessDev <jaess-dev@gmx.de>
2025-10-07 21:51:31 +0200

    add documentation

--- scripts/Util/Util.cs
+++ scripts/Util/Util.cs
@@ -11,12 +11,24 @@ public static class Util
         self.GetTree().ReloadCurrentScene();
     }
 
+    /// <summary>
+    /// Gets all nods of type T in the current scene tree starting with the root node.
+    /// </summary>
+    /// <typeparam name="T">The type of the nodes that should be found.</typeparam>
+    /// <param name="self">The node from which the scene tree and thus the root is accessed.</param>
+    /// <returns>A list of all nodes of type T in the current scene tree.</returns>
     public static List<T> GetNodesOfType<T>(this Node self)
-        where T : Node
         => self.GetTree().Root.GetNodesOfTypeRecur<T>();
 
+    /// <summary>
+    /// Recursively go through all nodes of the current scene tree adding them to the collection
+    /// if they are of the provided type T.
+    /// </summary>
+    /// <typeparam name="T">The type of which all nodes should be.</typeparam>
+    /// <param name="self">The node for which all it's children should be searched.</param>
+    /// <param name="coll">The collection into which the children are added.</param>
+    /// <returns>coll, mutating it to contain all nodes of type T.</returns>
     private static List<T> GetNodesOfTypeRecur<T>(this Node self, List<T> coll = null)
-        where T : Node
     {
         coll ??= [];
         if (self is T node) { coll.Add(node); }
