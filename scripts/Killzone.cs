fcff6200ac0a884452a49d977ba429c3c07447d2
JaessDev <jaess-dev@gmx.de>
2025-10-09 00:43:44 +0200

    properly listen to the event via UI bindings

--- scripts/Killzone.cs
+++ scripts/Killzone.cs
@@ -6,33 +6,29 @@ namespace Brackeys.Knight.Killzone;
 
 public partial class Killzone : Area2D
 {
-	public override void _Ready()
+	public void OnBodyEntered(Node2D body)
 	{
-		BodyEntered += (body) =>
+		var timer = GetNode<Timer>("Timer");
+		timer.Timeout += () =>
 		{
-			var timer = GetNode<Timer>("Timer");
-			timer.Timeout += () =>
-			{
-				Engine.TimeScale = 1;
-				this.GameOver();
-				QueueFree();
-			};
-
-
-			if (body is IYeetable player)
-			{
-				Printer.Print("You died!");
-				Engine.TimeScale = .5;
-				player.Yeet(new Vector2(200, -400));
-				timer.Start();
-			}
-#if DEBUG
-			else
-			{
-				Printer.Print("An object that was not yeetable entered the killzone");
-			}
-#endif
+			Engine.TimeScale = 1;
+			QueueFree();
 		};
+
+
+		if (body is IYeetable player)
+		{
+			Printer.Print("You died!");
+			Engine.TimeScale = .5;
+			player.Yeet(new Vector2(200, -400));
+			timer.Start();
+		}
+#if DEBUG
+		else
+		{
+			Printer.Print("An object that was not yeetable entered the killzone");
+		}
+#endif
 
 	}
 }
