be5f7a4f9f82ac1ee2014b32f56f06918472d3c9
JaessDev <jaess-dev@gmx.de>
2025-10-09 00:43:29 +0200

    use signals rather than relying on direct calls

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -1,17 +1,23 @@
 using Brackeys.Knight.Coin;
+using Brackeys.Knight.Hud;
 using Brackeys.Knight.Util;
 using Godot;
 using System;
 
 public partial class GameManager : Node
 {
+	[Signal]
+	public delegate void PointAddedEventHandler(int score);
+	[Signal]
+	public delegate void PlayerDiedEventHandler(int finalScore);
+
 	private int _score = 0;
 	private int _maxScore;
-	private Label _scoreLabel;
+	private Label ScoreLabel => GetNode<Label>("ScoreLabel");
+	private Hud Hud => GetNode<Hud>("%HUD");
 
 	public override void _Ready()
 	{
-		_scoreLabel = GetNode<Label>("ScoreLabel");
 		_maxScore = this.GetNodesOfType<Coin>().Count;
 
 		SetScoreText();
@@ -21,8 +27,20 @@ public partial class GameManager : Node
 	public void AddPoint()
 	{
 		_score++;
+
 		SetScoreText();
+		EmitSignal(SignalName.PointAdded, _score);
 	}
 
-	private void SetScoreText() => _scoreLabel.Text = $"You collected {_score} / {_maxScore} coins!";
+	private void SetScoreText() => ScoreLabel.Text = $"You collected {_score} / {_maxScore} coins!";
+
+	public void OnPlayerDeath()
+	{
+		EmitSignal(SignalName.PlayerDied, _score);
+	}
+
+	public void OnRestart()
+	{
+		this.GameOver();	
+	}
 }
