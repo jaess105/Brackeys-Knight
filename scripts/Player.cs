0640fff186c0cb390a2670259ac7c0275de2c469
JaessDev <jaess-dev@gmx.de>
2025-10-09 21:20:11 +0200

    use a state machine realized via a private interface encapsulating state based behaviour
    Using a private interface and private classes state based behaviour like the behaviour of the _PhisicsProcess method can be handled via polymorphism. The private classes can access private attributes of the enclosing task but separate the logic in different states.

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Brackeys.Knight.Util;
 using Godot;
 
@@ -8,8 +9,11 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 130.0f;
 	public const float JumpVelocity = -300.0f;
 
-
 	private AnimatedSprite2D _sprite;
+
+	private IPlayerState playerState;
+
+	public Player() { playerState = new NormalPlayerState(this); }
 
 	public override void _Ready()
 	{
@@ -18,73 +22,87 @@ public partial class Player : CharacterBody2D
 		_sprite.Play("idle");
 	}
 
-	private bool _playerEnabled = false;
-	public void EnablePlayer() => _playerEnabled = true;
-	public void DisablePlayer() => _playerEnabled = false;
+	public void EnablePlayer() => playerState = new NormalPlayerState(this);
+	public void DisablePlayer() => playerState = new DisabledPlayerState();
 
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (!_playerEnabled) { return; }
-		if (ExecuteYeeting((float)delta)) { return; }
-
-		Vector2 velocity = this.ApplyGravity((float)delta, this.Velocity);
-
-		Jump(ref velocity);
-
-		Vector2 direction = GetDirection();
-		Move(direction, ref velocity);
-		ChooseAnimation(direction);
-
-		Velocity = velocity;
-		MoveAndSlide();
+		playerState._PhysicsProcess(delta);
 	}
 
-
-	private void Jump(ref Vector2 velocity)
+	private interface IPlayerState
 	{
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+		void _PhysicsProcess(double delta);
 	}
 
-	private void Move(Vector2 direction, ref Vector2 velocity)
+	private class DisabledPlayerState : IPlayerState
 	{
-		// sprite direction facing change
-		if (direction == Vector2.Right)
+		public void _PhysicsProcess(double delta) { }
+	}
+
+	private class NormalPlayerState(Player player) : IPlayerState
+	{
+		public void _PhysicsProcess(double delta)
 		{
-			_sprite.FlipH = false;
-		}
-		else if (direction == Vector2.Left)
-		{
-			_sprite.FlipH = true;
+			Vector2 velocity = player.ApplyGravity((float)delta, player.Velocity);
+
+			Jump(ref velocity);
+
+			Vector2 direction = GetDirection();
+			Move(direction, ref velocity);
+			ChooseAnimation(direction);
+
+			player.Velocity = velocity;
+			player.MoveAndSlide();
 		}
 
 
-		// movement towards what direction
-		velocity.X = direction == Vector2.Zero
-			? Mathf.MoveToward(Velocity.X, 0, Speed)
-			: direction.X * Speed;
-	}
-
-	private void ChooseAnimation(Vector2 direction)
-	{
-		if (IsOnFloor())
+		private void Jump(ref Vector2 velocity)
 		{
-			_sprite.Play(direction == Vector2.Zero ? "idle" : "run");
-			return;
+			if (Input.IsActionJustPressed("jump") && player.IsOnFloor())
+			{
+				velocity.Y = JumpVelocity;
+			}
 		}
 
-		_sprite.Play("jump");
-	}
+		private void Move(Vector2 direction, ref Vector2 velocity)
+		{
+			// sprite direction facing change
+			if (direction == Vector2.Right)
+			{
+				player._sprite.FlipH = false;
+			}
+			else if (direction == Vector2.Left)
+			{
+				player._sprite.FlipH = true;
+			}
 
-	private static Vector2 GetDirection()
-	{
-		// Get the input direction and handle the movement/deceleration.
-		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		// ignore up and down.
-		return (direction == Vector2.Up || direction == Vector2.Down) ? Vector2.Zero : direction;
+
+			// movement towards what direction
+			velocity.X = direction == Vector2.Zero
+				? Mathf.MoveToward(velocity.X, 0, Speed)
+				: direction.X * Speed;
+		}
+
+		private void ChooseAnimation(Vector2 direction)
+		{
+			if (player.IsOnFloor())
+			{
+				player._sprite.Play(direction == Vector2.Zero ? "idle" : "run");
+				return;
+			}
+
+			player._sprite.Play("jump");
+		}
+
+		private static Vector2 GetDirection()
+		{
+			// Get the input direction and handle the movement/deceleration.
+			Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+			// ignore up and down.
+			return (direction == Vector2.Up || direction == Vector2.Down) ? Vector2.Zero : direction;
+		}
 	}
 }
 
@@ -97,13 +115,12 @@ public partial class Player : IYeetable
 	public delegate void PlayerDiedEventHandler();
 
 	private float _rotationSpeed = 0f;
-	private bool _yeet = false;
 
 	public async void Yeet(Vector2 impulse)
 	{
 		EmitSignal(SignalName.PlayerDied);
 
-		_yeet = true;
+		playerState = new YeetingPlayerState(this);
 		_rotationSpeed = Mathf.DegToRad(360); // one full spin per second
 
 		Velocity = impulse;
@@ -114,18 +131,16 @@ public partial class Player : IYeetable
 
 		await ToSignal(GetTree().CreateTimer(2.0), SceneTreeTimer.SignalName.Timeout);
 		DisablePlayer();
-		_yeet = false;
 	}
 
-	private bool ExecuteYeeting(float delta)
+	private class YeetingPlayerState(Player player) : IPlayerState
 	{
-		if (!_yeet) { return false; }
+		public void _PhysicsProcess(double delta)
+		{
+			player.Velocity += Vector2.Down * player.GetGravity() * (float)delta;
+			player.Rotation += player._rotationSpeed * (float)delta;
 
-
-		Velocity += Vector2.Down * GetGravity() * delta;
-		Rotation += _rotationSpeed * delta;
-
-		MoveAndSlide();
-		return true;
+			player.MoveAndSlide();
+		}
 	}
 }
